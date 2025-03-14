708870f8108b4ad0f205f03dfc6468a3f858ea46
Rahul Prakash <rahul.prakash02@sap.com>
2025-03-15 02:34:15 +0530

    .

--- kollygpt/kollyGPTPlots.py
+++ kollygpt/kollyGPTPlots.py
@@ -1,3 +1,4 @@
+import re
 from transformers import pipeline
 from kollygpt.GPT2Model import GPT2Model
 
@@ -5,8 +6,14 @@ class kollyGPTPlots:
     def __init__(self):
         self.tokenizer = GPT2Model().load_tokenizer()
         self.kollygpt = pipeline("text-generation", model="./gpt2-finetuned", tokenizer=self.tokenizer)
+    
+    def filter_english_words(text):
+        words = text.split()
+        filtered_words = [word for word in words if re.search(r'[A-Za-z]', word) and not re.search(r'[^A-Za-z.,!?]', word)]
+        return ' '.join(filtered_words)
 
     def plotter(self, lead, genre):
         prompt = f"Prompt: Suggest a plot for a {genre} movie starring {lead} Response: "
-        output = self.kollygpt(prompt, max_length=200, num_return_sequences=1)
-        return output[0]["generated_text"].replace(prompt, "")
+        output = self.kollygpt(prompt, max_length=200, num_return_sequences=1, temperature=0.75, top_k=50, top_p=0.9)
+        genPlot = output[0]["generated_text"].replace(prompt, "")
+        return self.filter_english_words(genPlot)
